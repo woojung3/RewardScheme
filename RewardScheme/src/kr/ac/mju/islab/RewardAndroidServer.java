67a25c868538298c5c0e409bc5803e2c9f7ac08d
jwlee <jwlee@jwlee-PC>
2017-04-07 13:36:35 +0900

    Terminate on connection closed bug fixed

--- RewardScheme/src/kr/ac/mju/islab/RewardAndroidServer.java
+++ RewardScheme/src/kr/ac/mju/islab/RewardAndroidServer.java
@@ -69,135 +69,145 @@ public class RewardAndroidServer implements Runnable {
     public void run() {
         InetSocketAddress sockAddr = new InetSocketAddress(bindPort);
         
-		ServerSocketChannel serverSock;
+		ServerSocketChannel serverSock = null;
 		try {
 			serverSock = ServerSocketChannel.open();
 			serverSock.socket().bind(sockAddr);
-				
-			while(true){
-				SocketChannel sockChannel = serverSock.accept();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+			
+		while(true){
+			SocketChannel sockChannel = null;
+			try {
+				sockChannel = serverSock.accept();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 
-				// Read
-				ByteBuffer buf = ByteBuffer.allocate(32768);	// Capable of dealing with #100 aggregated receipt
+			// Read
+			ByteBuffer buf = ByteBuffer.allocate(32768);	// Capable of dealing with #100 aggregated receipt
+			try {
 				sockChannel.read(buf);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 
-            	// Process header to get a length of received packet.
-            	byte[] packetLenInfo = new byte[4];
-				buf.flip();
-				try{
-					buf.get(packetLenInfo);
-				} catch (Exception e) { }	// Ignore errors on client disconnection.
-				buf.compact();
-            	
-				int packetLen = ByteBuffer.wrap(packetLenInfo).getInt();
-				if (packetLen == 0) {
-					return;
-				}
-				byte[] recvBytePacket = new byte[packetLen];
-				if (recvBytePacket.length > 32768) {
-					System.out.println("Buffer size is smaller than received packet size: " + recvBytePacket.length);
-				}
-				buf.flip();
-				buf.get(recvBytePacket);
-				buf.compact();
-				// END
+			// Process header to get a length of received packet.
+			byte[] packetLenInfo = new byte[4];
+			buf.flip();
+			try{
+				buf.get(packetLenInfo);
+			} catch (Exception e) { }	// Ignore errors on client disconnection.
+			buf.compact();
+			
+			int packetLen = ByteBuffer.wrap(packetLenInfo).getInt();
+			if (packetLen == 0) {
+				continue;
+			}
+			byte[] recvBytePacket = new byte[packetLen];
+			if (recvBytePacket.length > 32768) {
+				System.out.println("Buffer size is smaller than received packet size: " + recvBytePacket.length);
+			}
+			buf.flip();
+			buf.get(recvBytePacket);
+			buf.compact();
+			// END
 
-                recvPacket = null;
-                RewardPacket sendPacket = null;
-				try {
-					recvPacket = RewardPacket.parseFrom(recvBytePacket);
-				} catch (InvalidProtocolBufferException e) {
-					e.printStackTrace();
-				}
+			recvPacket = null;
+			RewardPacket sendPacket = null;
+			try {
+				recvPacket = RewardPacket.parseFrom(recvBytePacket);
+			} catch (InvalidProtocolBufferException e) {
+				e.printStackTrace();
+			}
 
-				// Process packet according to its pid.
-				if (recvPacket.getPid() == 1) { // RewardScheme.recIssueMaster
-					if (this.onIssueRequest() == false) {	// Notifying that the protocol is refused.
-						sendPacket = RewardPacket.newBuilder()
-								.setPid(999)
-								.build();
-					}
-					else {
-						Element h = rewardScheme.G1.newElementFromBytes(recvPacket.getE1().toByteArray()).getImmutable();
-						Element psi = rewardScheme.recIssueMaster(h).getImmutable();
-
-						sendPacket = RewardPacket.newBuilder()
-								.setPid(1)	// Is not necessary. Append for analysis
-								.setE1(ByteString.copyFrom(psi.toBytes()))
-								.build();
-					}
-				}
-				else if (recvPacket.getPid() == 2) {	// RewardScheme.verify
-					if (this.onVerifyRequest() == false) {	// Notifying that the protocol is refused.
-						sendPacket = RewardPacket.newBuilder()
-								.setPid(999)
-								.build();
-					}
-					else {
-						Element sigma = rewardScheme.G1.newElementFromBytes(recvPacket.getE1().toByteArray()).getImmutable();
-						Element s = rewardScheme.Zr.newElementFromBytes(recvPacket.getE2().toByteArray()).getImmutable();
-						Element y = rewardScheme.G2.newElementFromBytes(recvPacket.getE3().toByteArray()).getImmutable();
-						
-						sendPacket = RewardPacket.newBuilder()
-								.setPid(2)	// Is not necessary. Append for analysis
-								.setIsValid(rewardScheme.verify(sigma, s, y))
-								.build();
-					}
-				}
-				else if (recvPacket.getPid() == 3) {	// RewardScheme.aggVerify
-					if (this.onAggVerifyRequest() == false) {	// Notifying that the protocol is refused.
-						sendPacket = RewardPacket.newBuilder()
-								.setPid(999)
-								.build();
-					}
-					else {
-						Element sigmaAgg = rewardScheme.G1.newElementFromBytes(recvPacket.getE1().toByteArray()).getImmutable();
-						List<Element> sList = ByteStringListToElementList(recvPacket.getEList1List(), rewardScheme.Zr);
-						List<Element> yList = ByteStringListToElementList(recvPacket.getEList2List(), rewardScheme.G2);
-						
-						sendPacket = RewardPacket.newBuilder()
-								.setPid(3)	// Is not necessary. Append for analysis
-								.setIsValid(rewardScheme.aggVerify(sigmaAgg, sList, yList))
-								.build();
-					}
-				}
-				else if (recvPacket.getPid() == 101) {	// RewardScheme.y
-					if (this.onYRequest() == false) {	// Notifying that the protocol is refused.
-						sendPacket = RewardPacket.newBuilder()
-								.setPid(999)
-								.build();
-					}
-					else {
-						sendPacket = RewardPacket.newBuilder()
-								.setPid(101)	// Is not necessary. Append for analysis
-								.setE1(ByteString.copyFrom(rewardScheme.y.toBytes()))
-								.build();
-					}
+			// Process packet according to its pid.
+			if (recvPacket.getPid() == 1) { // RewardScheme.recIssueMaster
+				if (this.onIssueRequest() == false) {	// Notifying that the protocol is refused.
+					sendPacket = RewardPacket.newBuilder()
+							.setPid(999)
+							.build();
 				}
 				else {
-					System.err.println("Unexpected: " + recvPacket.getAllFields());
-					return;
-				}
+					Element h = rewardScheme.G1.newElementFromBytes(recvPacket.getE1().toByteArray()).getImmutable();
+					Element psi = rewardScheme.recIssueMaster(h).getImmutable();
 
-				// Write
-				// Prepend packetLenInfo to the RewardPacket, so that the other side can determine the packet size
-				int sendPacketLen = sendPacket.toByteArray().length;
-				byte[] sendPacketLenInfo = ByteBuffer.allocate(4).putInt(sendPacketLen).array();
-
-				ByteBuffer sendBuf = ByteBuffer.allocate(4 + sendPacketLen);
-				sendBuf.put(sendPacketLenInfo);
-				sendBuf.put(sendPacket.toByteArray());
-				sendBuf.flip();
-                // END
-				
-				while (sendBuf.hasRemaining()) {
-					sockChannel.write(sendBuf);
+					sendPacket = RewardPacket.newBuilder()
+							.setPid(1)	// Is not necessary. Append for analysis
+							.setE1(ByteString.copyFrom(psi.toBytes()))
+							.build();
 				}
-				
-				sockChannel.close();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			else if (recvPacket.getPid() == 2) {	// RewardScheme.verify
+				if (this.onVerifyRequest() == false) {	// Notifying that the protocol is refused.
+					sendPacket = RewardPacket.newBuilder()
+							.setPid(999)
+							.build();
+				}
+				else {
+					Element sigma = rewardScheme.G1.newElementFromBytes(recvPacket.getE1().toByteArray()).getImmutable();
+					Element s = rewardScheme.Zr.newElementFromBytes(recvPacket.getE2().toByteArray()).getImmutable();
+					Element y = rewardScheme.G2.newElementFromBytes(recvPacket.getE3().toByteArray()).getImmutable();
+					
+					sendPacket = RewardPacket.newBuilder()
+							.setPid(2)	// Is not necessary. Append for analysis
+							.setIsValid(rewardScheme.verify(sigma, s, y))
+							.build();
+				}
+			}
+			else if (recvPacket.getPid() == 3) {	// RewardScheme.aggVerify
+				if (this.onAggVerifyRequest() == false) {	// Notifying that the protocol is refused.
+					sendPacket = RewardPacket.newBuilder()
+							.setPid(999)
+							.build();
+				}
+				else {
+					Element sigmaAgg = rewardScheme.G1.newElementFromBytes(recvPacket.getE1().toByteArray()).getImmutable();
+					List<Element> sList = ByteStringListToElementList(recvPacket.getEList1List(), rewardScheme.Zr);
+					List<Element> yList = ByteStringListToElementList(recvPacket.getEList2List(), rewardScheme.G2);
+					
+					sendPacket = RewardPacket.newBuilder()
+							.setPid(3)	// Is not necessary. Append for analysis
+							.setIsValid(rewardScheme.aggVerify(sigmaAgg, sList, yList))
+							.build();
+				}
+			}
+			else if (recvPacket.getPid() == 101) {	// RewardScheme.y
+					sendPacket = RewardPacket.newBuilder()
+							.setPid(101)	// Is not necessary. Append for analysis
+							.setE1(ByteString.copyFrom(rewardScheme.y.toBytes()))
+							.build();
+			}
+			else {
+				System.err.println("Unexpected: " + recvPacket.getAllFields());
+				return;
+			}
+
+			// Write
+			// Prepend packetLenInfo to the RewardPacket, so that the other side can determine the packet size
+			int sendPacketLen = sendPacket.toByteArray().length;
+			byte[] sendPacketLenInfo = ByteBuffer.allocate(4).putInt(sendPacketLen).array();
+
+			ByteBuffer sendBuf = ByteBuffer.allocate(4 + sendPacketLen);
+			sendBuf.put(sendPacketLenInfo);
+			sendBuf.put(sendPacket.toByteArray());
+			sendBuf.flip();
+			// END
+			
+			while (sendBuf.hasRemaining()) {
+				try {
+					sockChannel.write(sendBuf);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			try {
+				sockChannel.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
     }
 
